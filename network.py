d34953753d4daa8456acda86ba3a74317eb9c1c8
Ashutosh <30094761+agojiya@users.noreply.github.com>
2017-10-22 23:10:56 -0400

    config

--- network.py
+++ network.py
@@ -1,11 +1,16 @@
 import ipfsapi
+import configparser
 
 class NetworkLayer(object):
 	
 	def upload_encrypted_chunks(hiddendirectory, encrypted_file_list):
 		# ipfs add the encrypted file:: Make this configurable
+		cfg_parser = configparser.RawConfigParser()
+		cfg_parser.read('config.cfg')
+
 		file_to_hash_list = []
-		api = ipfsapi.connect('127.0.0.1', 5001)
+		ip, port = cfg_parser['Network']['IP'], int(cfg_parser['Network']['Port'])
+		api = ipfsapi.connect(ip, port)
 		for file in encrypted_file_list:
 			filename = hiddendirectory + '/' + file
 			res = api.add(filename)
